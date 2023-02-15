60c8c33b4f398f8d19e4f1c37defee63575e0165
Nicolas Olaya <111377816+NicolasAndrehh@users.noreply.github.com>
2023-02-15 10:33:17 -0500

    Add navigation with JS

--- main.js
+++ main.js
@@ -18,6 +18,8 @@ if (localStorage.getItem('inputTitle')) {
 
 // Add book button event listener
 const addBookButton = document.querySelector('#add-book-button');
+const confirmationMessage = document.querySelector('.confirmation-message');
+let cont = 1;
 addBookButton.addEventListener('click', (e) => {
   const title = document.querySelector('.title-input').value;
   const author = document.querySelector('.author-input').value;
@@ -27,7 +29,42 @@ addBookButton.addEventListener('click', (e) => {
     localStorage.setItem('books', JSON.stringify(bookList.getBooks()));
     localStorage.setItem('inputTitle', title);
     localStorage.setItem('inputAuthor', author);
+    confirmationMessage.innerHTML = `${cont} book(s) were added succesfully`;
+    cont += 1;
   }
 });
 
-//
+// Navigation
+const showBooksSection = document.querySelector('.show-books-section');
+const addBooksSection = document.querySelector('.add-books-section');
+const contactSection = document.querySelector('.contact-section');
+
+function showBookListSection() {
+  addBooksSection.classList.add('hide');
+  contactSection.classList.add('hide');
+  showBooksSection.classList.remove('hide');
+  confirmationMessage.innerHTML = '';
+  cont = 1;
+}
+
+function showAddBookSection() {
+  contactSection.classList.add('hide');
+  showBooksSection.classList.add('hide');
+  addBooksSection.classList.remove('hide');
+}
+
+function showContactSection() {
+  addBooksSection.classList.add('hide');
+  showBooksSection.classList.add('hide');
+  contactSection.classList.remove('hide');
+  confirmationMessage.innerHTML = '';
+  cont = 1;
+}
+
+const bookListShowButton = document.querySelector('.book-list-show');
+const addBookShowButton = document.querySelector('.add-book-show');
+const contactShowButton = document.querySelector('.contact-show');
+
+bookListShowButton.addEventListener('click', showBookListSection);
+addBookShowButton.addEventListener('click', showAddBookSection);
+contactShowButton.addEventListener('click', showContactSection);
